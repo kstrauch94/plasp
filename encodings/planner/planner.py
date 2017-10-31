7ed7051c6ae53b749b4f66296140f958c65777b0
kstrauch94 <kstrauch0694@gmail.com>
2017-10-31 17:26:03 +0100

    save old planner and basic, add DLP to new planner

--- encodings/planner/planner.py
+++ encodings/planner/planner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 
 #
@@ -12,6 +12,8 @@ import re
 from time import clock
 import clingo_stats
 import os
+
+import ClingoGrounder
 
 # 
 # DEFINES
@@ -287,9 +289,9 @@ class C_Scheduler(Scheduler):
             self.__first  = False
 
         # NONE: check if all Nones, append and pop
-        elif result == NO_MEM:
+        elif result != NO_MEM:
             self.__nones.add(self.__runs[0])
-            if len(self.__nones) == len(self.__runs):
+            if len(self.__nones) == len(self.__runs): 
                 return None
             self.__runs.append(self.__runs[0])
             self.__runs = self.__runs[1:]
@@ -339,6 +341,10 @@ FORBID_ACTIONS_PROGRAM = """
 """
 FORCE_ACTIONS_PROGRAM = """
 #program step(t).
+:- not occurs(A,t):action(A); not skip(t). % some action
+"""
+FORCE_ACTIONS_PROGRAM_OLD = """
+#program step(t).
 :- not occurs(_,t), not skip(t). % some action
 """
 BLOCK_BASE = "block_base"   # block_base
@@ -354,11 +360,10 @@ _test(A,B) :- #false, _test(A,B).
 
 class Tester:
 
-    def __init__(self, ctl, _files, print_errors, test_once):
+    def __init__(self, ctl, _files, print_errors):
         self.__ctl = ctl
         self.__files = _files
         self.__print_errors = print_errors
-        self.__test_once = test_once
         # rest
         self.__shown = None
         self.__model = 0
@@ -375,9 +380,6 @@ class Tester:
         return self.__shown
 
     def test(self, shown, length):
-        if self.__model and self.__test_once:
-            log("SERIALIZABLE?", PRINT)
-            return True
         # do test
         ctl = clingo.Control(["--warn=none"])#, "--output-debug=text"])
         for i in self.__files:
@@ -441,13 +443,11 @@ class Solver:
 
         # tester
         self.__test = False if options['test'] is None else True
-        self.__run_test = self.__test
         self.__shown = None
         self.__tester = None
         if self.__test:
             self.__tester = Tester(
-                self.__ctl, options['test'],
-                options['print_errors'], options['test_once']
+                self.__ctl, options['test'], options['print_errors']
             )
 
     def __print_model(self, shown):
@@ -562,28 +562,18 @@ class Solver:
         self.__last_length = length
 
         # test
-        if result.satisfiable and self.__run_test:
-            while True:
-                log("Testing...", PRINT)
-                if self.__verbose: self.__verbose_start()
-                # run the test
-                test_result = self.__tester.test(self.__shown, self.__length)
-                if self.__verbose: self.__verbose_end("Testing")
-                # if passed, return SAT
-                if test_result:
-                    self.__print_model(self.__shown)
-                    return SATISFIABLE
-                # return UNKNOWN
-                if not self.__options["test_until_not_sat"]:
-                    return UNKNOWN
-                # unless test_until_unsat
-                log("Solving...", PRINT)
-                if self.__verbose: self.__verbose_start()
-                result = self.__ctl.solve(on_model=self.__on_model)
-                if self.__verbose: self.__verbose_end("Solving")
-                log(str(result))
-                if not result.satisfiable:
-                    break
+        if result.satisfiable and self.__test:
+            log("Testing...", PRINT)
+            if self.__verbose: self.__verbose_start()
+            test_result = self.__tester.test(self.__shown, self.__length)
+            if self.__verbose: self.__verbose_end("Testing")
+            if self.__options['test_once']:
+                self.__test = False
+            if test_result:
+                self.__print_model(self.__shown)
+                return SATISFIABLE
+            else:
+                return UNKNOWN
 
         # return
         if result.satisfiable:
@@ -593,7 +583,167 @@ class Solver:
         else:
             return UNKNOWN
 
+class SolverDLP:
 
+    def __init__(self, dlp, options):
+
+        self.__dlp         = dlp
+        self.__length      = 0
+        self.__last_length = 0
+        self.__options     = options
+        self.__verbose     = options['verbose']
+        if self.__verbose: self.__memory = memory_usage()
+        self.__models      = 0
+
+        # mem
+        self.__mem         = True if options['check_mem'] else False
+        self.__mem_limit   = options['check_mem']*0.9
+        self.__mem_max     = 0
+        self.__mem_before  = 0
+
+        self.__move_query = options['move_query'] 
+
+        # tester
+        self.__test = False if options['test'] is None else True
+        self.__shown = None
+        self.__tester = None
+        if self.__test:
+            self.__tester = Tester(
+                self.__ctl, options['test'], options['print_errors']
+            )
+
+    def __print_model(self, shown):
+        if self.__options['outf'] == 0:
+            log("Answer: {}\n{}".format(self.__models, " ".join(shown)), PRINT)
+        else:
+            line = " ".join([x + "." for x in shown])
+            log("ANSWER\n{}".format(line), FORCE_PRINT)
+
+    def __on_model(self, m):
+        self.__models += 1
+        shown = [ str(x) for x in m.symbols(shown = True) ]
+        if self.__test:
+            if self.__options['print_nonserial']:
+                self.__print_model(shown)
+            self.__shown = shown
+        else:
+            self.__print_model(shown)
+
+    # to get rid of clingo error when using the tester
+    def get_shown(self):
+        return []
+
+    def get_models(self):
+        return self.__models
+
+    def __verbose_start(self):
+        self.__time0 = clock()
+
+    def __verbose_end(self, string):
+        log(string+" Time:\t {:.2f}s".format(clock()-self.__time0))
+        memory = memory_usage()
+        if self.__memory == -1 or memory == -1: 
+            return
+        log("Memory:\t\t "+str(memory)+"MB (+"+str(memory-self.__memory)+"MB)")
+        self.__memory = memory
+
+    def __mem_check_limit(self, length):
+        self.__mem_before = memory_usage("VmSize")
+        log("Expected Memory: {}MB".format(
+            self.__mem_before + (self.__mem_max*length)
+        ))
+        if self.__mem_limit < (self.__mem_before + (self.__mem_max*length)):
+            return True
+        return False
+
+    def __mem_set_max(self, length):
+        self.__mem_max = max(self.__mem_max, (
+            memory_usage("VmSize") - self.__mem_before
+        )/float(length))
+
+    def solve(self, length):
+
+        log("Grounded Until:\t {}".format(self.__length))
+
+        # ground if necessary
+        grounded = 0
+        if self.__length < length:
+            if self.__mem and self.__mem_check_limit(length-self.__length):
+                log("Skipping: not enough memory for grounding...\n")
+                return NO_MEM
+
+            if not self.__move_query:
+                self.__dlp.release_external(
+                    clingo.Function(QUERY,[self.__length])
+                )
+            log("Grounding...\t " + str(self.__length+1)+ ", " + str(length))
+            if self.__verbose: self.__verbose_start()
+            if self.__test:
+                parts += self.__tester.get_parts(self.__length+1, length+1)
+
+            self.__dlp.ground(self.__length+1, length)
+
+            if self.__verbose: self.__verbose_end("Grounding")
+            for t in range(self.__length + 1, length):
+                self.__dlp.release_external(
+                    clingo.Function(QUERY, [t])
+                )
+            if not self.__move_query:
+                self.__dlp.assign_external(
+                    clingo.Function(QUERY,[length]),True
+                )
+            self.__dlp.cleanup()
+            grounded      = length - self.__length
+            self.__length = length
+
+        # blocking or unblocking actions
+        if length < self.__last_length:
+            log("Blocking actions...")
+            for t in range(length+1, self.__last_length+1):
+                self.__dlp.assign_external(clingo.Function(SKIP,[t]), True)
+        elif self.__last_length < length:
+            log("Unblocking actions...")
+            for t in range(self.__last_length+1, length+1):
+                self.__dlp.assign_external(clingo.Function(SKIP,[t]), False)
+
+        # solve
+        log("Solving...", PRINT)
+        if self.__verbose: self.__verbose_start()
+        if self.__move_query:
+            self.__dlp.assign_external(
+                clingo.Function(QUERY,[self.__last_length]), False
+            )
+            self.__dlp.assign_external(
+                clingo.Function(QUERY,[length]), True
+            )
+        result = self.__dlp.solve(on_model=self.__on_model)
+        if self.__verbose: self.__verbose_end("Solving")
+        log(str(result))
+        if self.__mem and grounded:
+            self.__mem_set_max(grounded)
+        self.__last_length = length
+
+        # test
+        if result.satisfiable and self.__test:
+            log("Testing...", PRINT)
+            if self.__verbose: self.__verbose_start()
+            test_result = self.__tester.test(self.__shown, self.__length)
+            if self.__verbose: self.__verbose_end("Testing")
+            if self.__options['test_once']:
+                self.__test = False
+            if test_result:
+                self.__print_model(self.__shown)
+                return SATISFIABLE
+            else:
+                return UNKNOWN
+
+        # return
+        if result.satisfiable:
+            return SATISFIABLE
+        elif result.unsatisfiable:
+            return UNSATISFIABLE
+        else:
+            return UNKNOWN
 #
 # PLANNER
 #
@@ -601,31 +751,41 @@ class Solver:
 class Planner:
 
     def run(self,options,clingo_options):
-
-        time0 = clock()
-        ctl = clingo.Control(clingo_options)
+        
+        files = []
+        program = ""
 
         # input files
+
+        instance_name = "instance-name-test.lp"
+
         for i in options['files']:
-            ctl.load(i)
+            files.append(i)
         if options['read_stdin']:
-            ctl.add(BASE, [], get_stdin())
-
+            #with open(instance_name, "w") as f:
+            #    f.write(get_stdin())
+            #files.append(instance_name)
+            program += get_stdin()
+        
         # additional programs
-        ctl.add(BASE,[],EXTERNALS_PROGRAM)
-        if options['test']:
-            ctl.add(BASE,[],TEST_NO_WARNING)
-        if options['forbid_actions']: 
-            ctl.add(BASE, [], FORBID_ACTIONS_PROGRAM)
-        if options['force_actions']:  
-            ctl.add(BASE, [], FORCE_ACTIONS_PROGRAM)
 
-        # ground base, and set initial query
-        ctl.ground([(BASE,[]), (CHECK,[0])])
-        ctl.assign_external(clingo.Function(QUERY,[0]), True)
+        program += EXTERNALS_PROGRAM
+        if options['test']:
+            program += TEST_NO_WARNING
+        if options['forbid_actions']: 
+            program += FORBID_ACTIONS_PROGRAM
+        if options['force_actions']:  
+            program += FORCE_ACTIONS_PROGRAM
+
+        #dlp = ClingoGrounder.DynamicLogicProgramText(files, program, options, clingo_options)
+        dlp = ClingoGrounder.DynamicLogicProgramBasic(files, program, options, clingo_options)
+
+        dlp.start()
+
+        dlp.assign_external(clingo.Function(QUERY,[0]), True)
 
         # solver
-        solver = Solver(ctl,options)
+        solver = SolverDLP(dlp,options)         
 
         # scheduler
         # check argument error
@@ -657,8 +817,7 @@ class Planner:
         verbose = options['verbose']
         memory = memory_usage()
         if verbose and memory!=-1:
-            log("\nTime:\t {:.2f}s".format(clock()- time0))
-            log("Memory: {}MB\n".format(memory))
+            log("\nMemory: {}MB\n".format(memory))
 
         # loop
         i=1
@@ -684,11 +843,15 @@ class Planner:
             #log("\n" + clingo_stats.Stats().summary(ctl),PRINT)
             #if options['stats']:
             #    log(clingo_stats.Stats().statistics(ctl),PRINT)
+            if i==12:
+                pass #break
 
         # stats
-        log("\n" + clingo_stats.Stats().summary(ctl),PRINT)
+
+        log("\n" + clingo_stats.Stats().summary(dlp.control),PRINT)
+        log("\nDLP grounding time: " + str(dlp.ground_time), PRINT)
         if options['stats']:
-            log(clingo_stats.Stats().statistics(ctl),PRINT)
+            log(clingo_stats.Stats().statistics(dlp.control),PRINT)
             # peak memory
             peak = memory_usage("VmPeak")
             if peak != -1: 
@@ -699,8 +862,6 @@ class Planner:
             if options['test']:
                 log("Models       : {}".format(solver.get_models()), PRINT)
             log("",PRINT)
-            
-
 
 
 #
@@ -782,10 +943,6 @@ Get help/report bugs via : https://potassco.org/support
         solving.add_argument(
             '--test-once', dest='test_once', action="store_true",
             help="Test solution only once (using option --test)"
-        )
-        solving.add_argument(
-            '--test-until-not-sat', dest='test_until_not_sat', action="store_true",
-            help="Test solutions of one time point until not SAT (using option --test)"
         )
         solving.add_argument(
             '--query-at-last',dest='move_query',action="store_false",
