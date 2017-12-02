cc0095cc50f522acd54ae55010c3b98eadaf34ec
kstrauch94 <kstrauch0694@gmail.com>
2017-12-02 16:59:25 +0100

    small updates

--- encodings/planner/run.sh
+++ encodings/planner/run.sh
@@ -10,15 +10,17 @@ INSTANCE4=ins3.lp
 
 INSTANCE5=/home/klaus/Desktop/Work/Efficient-grounding/benchmarks/instances-easy/ipc-2000/domains/blocks-strips-typed/instances/instance-12.pddl
 
-# test
-#python -B runplanner.py $INSTANCE1 --translate --parallel=5
+# basic
+#python -B runplanner.py $INSTANCE2 --translate --parallel=5
 
 # text DLP
-#python -B runplanner.py $INSTANCE2 --translate --dlp=text --parallel=5
-# text DLP
-python -B runplanner.py $INSTANCE1 --translate --dlp=backend --parallel=5
-# text DLP
-#python -B runplanner.py $INSTANCE5 --translate --dlp=backend-simplified
+python -B runplanner.py $INSTANCE1 --translate --dlp=text --parallel=5 --stats
+# backend DLP
+#python -B runplanner.py $INSTANCE1 --translate --dlp=backend --parallel=5
+# backend simplified DLP
+#python -B runplanner.py $INSTANCE1 --translate --dlp=backend-simplified --parallel=5
+# backend simplified NCNB DLP
+#python -B runplanner.py $INSTANCE1 --translate --dlp=backend-simplified-ncnb --parallel=5
 
 #python runplanner.py $INSTANCE1 --translate --parallel=0 --heuristic=Domain heuristic.lp --verbose
 #python planner.py --forbid-actions $INSTANCE4 basic.lp --verbose #--output-debug=text
